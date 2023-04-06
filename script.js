9f11c4cc0f42e10e252c2f7528bc6a9a6fa4e3ef
amankumar110 <115738540+amankumar110@users.noreply.github.com>
2023-04-07 02:37:22 +0500

    v3 complete
    added option to rate the image

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const BASE_URL = "https://api.unsplash.com/photos/";
 const container = document.querySelector(".container");
 const loader = document.querySelector("#loader");
-let pagecount = 1;
-const img_per_page = 4;
-let topheading = 1;
+let pagecount = Math.floor(Math.random() * 30);
+let img_per_page = 4;
+let details = navigator.userAgent;
 let imageOrientation = "portrait";
-
-const details = navigator.userAgent;
-const regexp = /android|iphone|kindle|ipad/i;
-
-const isMobileDevice = regexp.test(details);
+let regexp = /android|iphone|kindle|ipad/i;
+let isMobileDevice = regexp.test(details)
+if (isMobileDevice) {
+  img_per_page = 2;
+}
 
 async function getImages() {
   try {
@@ -17,8 +17,9 @@ async function getImages() {
     const res = await fetch(
       `${BASE_URL}?orientation=${imageOrientation}&page=${pagecount}&per_page=${img_per_page}&client_id=${API_KEY}`
     );
-    const data = await res.json();
-    return data;
+      const data = await res.json();
+      return data;
+    
   } catch (e) {
     console.log("Error: " + e);
     return null;
@@ -26,16 +27,40 @@ async function getImages() {
 }
 
 async function showImages() {
+  loader.style.display = "block";
   const images = await getImages();
   if (images !== null) {
     for (const img of images) {
-      const imgUrl = isMobileDevice ? img.urls.regular : img.urls.regular;
-      const html = document.createElement("img");
-      html.src = imgUrl;
-      container.insertAdjacentElement("beforeend", html);
-      html.classList.add("border");
+      const imghtml = document.createElement("img");
+     const likediv = createLikeContainer();
+      imgUrl = img.urls.regular;
+      imghtml.src = imgUrl;
+      imghtml.alt = img.alt_description;
+      container.insertAdjacentElement("beforeend", imghtml);
+      imghtml.classList.add("border");
+      imghtml.insertAdjacentElement("afterend",likediv);
+      likediv.style.display = "flex"
     }
+  } else {
+    container.textContent = "Something went wrong";
+    loader.style.display = "none";
   }
+};
+function createLikeContainer(){
+  const div = document.createElement("div");
+  div.classList.add("likediv");
+  const ratinginfo = "Rate this image";
+  div.append(ratinginfo);
+  const select = document.createElement("select");
+  select.value = "Rate";
+  div.appendChild(select);
+  for (let i = 1; i < 6; i++) {
+    const option = document.createElement("option");
+    option.textContent = i ;
+    option.value = i;
+    select.appendChild(option);
+  }
+  return div;
 }
 function update_page_count() {
   setTimeout(() => {
@@ -51,5 +76,4 @@ window.addEventListener("scroll", () => {
     update_page_count();
   }
 });
-
 showImages();
